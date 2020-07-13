b4f40d769e25846a0123acbe7487c0a5be9ef3b6
Afonso -aptsharp <aff-on@hotmail.com>
2020-07-12 23:39:51 -0300

    Primeiros passos - Estrutura de projeto -> Erros - Imagen não esta puxando do projeto, imagen do Rick como paleativo. =D

---
+++ Page_XF/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Page_XF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Page_XF.TipoPagina.Carousel.TipoPage1();
+            //para mostrar a pagina no proprio computador sem precisar passar para o celular.
+
+            //MainPage = new MainPage();
+            //default
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Page_XF/TipoPagina/Carousel/TipoPage3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Page_XF.TipoPagina.Carousel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TipoPage3 : ContentPage
+    {
+        public TipoPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
